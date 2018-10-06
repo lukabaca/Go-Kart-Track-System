80f4b33bca1c57ec7600fe8fc0896abb7efe031f
lukabaca <lkszblaszczyk@gmail.com>
2018-10-06 20:47:44 +0200

    refactored with registration and getting roles

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -37,39 +37,85 @@ class UserRepository extends EntityRepository implements UserLoaderInterface
         $conn= $this->getEntityManager()->getConnection();
 
         //gdy bedziemy miec 2 role dla 1 uzytkownika to musisz zwrocic 2 row set z rolami odzielnie
-        $sql = '
-            select user.id, password, user.name as userName, surname, birth_date, pesel, document_id, email, telephone_number, role.name as roleName from user  
-            join user_roles on user.id = user_roles.user_id join role  on role.id = user_roles.role_id
-            where user.email = ?
-        ';
+//        $sql = '
+//            select user.id, password, user.name as userName, surname, birth_date, pesel, document_id, email, telephone_number, role.name as roleName from user
+//            join user_roles on user.id = user_roles.user_id join role  on role.id = user_roles.role_id
+//            where user.email = ?
+//        ';
+
+        $sql1 = 'call getUserByUserEmail(?)';
+        $sql2 = 'call getUserRolesByUserID(?)';
+        $conn = $this->getEntityManager()->getConnection();
         try {
-            $stmt = $conn->prepare($sql);
-            $stmt->bindParam(1, $username);
+            $stmt = $conn->prepare($sql1);
+            $stmt->bindValue(1, $username);
             $rowCount = $stmt->execute();
             if($rowCount == 1) {
-                $temp = $stmt->fetchAll();
-//                return $temp[0];
-                $user = new User();
+                $tempUser = $stmt->fetchAll();
+                $userID = $tempUser[0]['id'];
 
-                $user->setId($temp[0]['id']);
-                $user->setPassword(($temp[0]['password']));
-                $user->setName($temp[0]['userName']);
-                $user->setSurname($temp[0]['surname']);
-                $user->setBirthDate($temp[0]['birth_date']);
-                $user->setPesel($temp[0]['pesel']);
-                $user->setDocumentID($temp[0]['document_id']);
-                $user->setEmail( $temp[0]['email']);
-                $user->setTelephoneNumber($temp[0]['telephone_number']);
+                $stmt = $conn->prepare($sql2);
+                $stmt->bindValue(1, $userID);
+                $rowCount = $stmt->execute();
+                if($rowCount > 0) {
+                    $tempRoles = $stmt->fetchAll();
 
-                $user->setRoles(array($temp[0]['roleName']));
+                    $user = new User();
 
-                return $user;
-            } else {
-                return null;
+                    $user->setId($tempUser[0]['id']);
+                    $user->setPassword(($tempUser[0]['password']));
+                    $user->setName($tempUser[0]['name']);
+                    $user->setSurname($tempUser[0]['surname']);
+                    $user->setBirthDate($tempUser[0]['birth_date']);
+                    $user->setPesel($tempUser[0]['pesel']);
+                    $user->setDocumentID($tempUser[0]['document_id']);
+                    $user->setEmail( $tempUser[0]['email']);
+                    $user->setTelephoneNumber($tempUser[0]['telephone_number']);
+
+                    $roles = [];
+                    foreach($tempRoles as $role) {
+                        $roles [] = $role['name'];
+                    }
+                    $user->setRoles($roles);
+
+                    return $user;
+                } else {
+                    return null;
+                }
             }
+
         } catch (DBALException $e) {
             return null;
         }
+
+//        try {
+//            $stmt = $conn->prepare($sql);
+//            $stmt->bindParam(1, $username);
+//            $rowCount = $stmt->execute();
+//            if($rowCount == 1) {
+//                $temp = $stmt->fetchAll();
+////                return $temp[0];
+//                $user = new User();
+//
+//                $user->setId($temp[0]['id']);
+//                $user->setPassword(($temp[0]['password']));
+//                $user->setName($temp[0]['userName']);
+//                $user->setSurname($temp[0]['surname']);
+//                $user->setBirthDate($temp[0]['birth_date']);
+//                $user->setPesel($temp[0]['pesel']);
+//                $user->setDocumentID($temp[0]['document_id']);
+//                $user->setEmail( $temp[0]['email']);
+//                $user->setTelephoneNumber($temp[0]['telephone_number']);
+//
+//                $user->setRoles(array($temp[0]['roleName']));
+//
+//                return $user;
+//            } else {
+//                return null;
+//            }
+//        } catch (DBALException $e) {
+//            return null;
+//        }
 
     }
 }
