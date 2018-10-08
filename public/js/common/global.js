447d0cb6c646af8e5e421cc301ef47fe5fc6f9fe
lukabaca <lkszblaszczyk@gmail.com>
2018-10-08 21:44:57 +0200

    working

--- public/js/common/global.js
+++ public/js/common/global.js
@@ -2,3 +2,12 @@
 //    e.preventDefault();
 //    window.history.back();
 // });
+
+$(document).ready(function () {
+    $('.userIcon').on('click', function (e) {
+        e.preventDefault();
+        console.log('klik');
+    });
+
+
+});
