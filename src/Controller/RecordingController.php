486b333b85aadba8c7642a040a17c40b06255830
lukabaca <lkszblaszczyk@gmail.com>
2018-10-13 14:16:11 +0200

    working

--- src/Controller/RecordingController.php
+++ src/Controller/RecordingController.php
@@ -9,8 +9,10 @@
 namespace App\Controller;
 use App\Entity\Recording;
 use App\Entity\User;
+use App\Form\RecordingType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -22,8 +24,41 @@ class RecordingController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('views/controllers/recording/index.html.twig', []
+        $recording = new Recording();
+//        return $this->handleForm($request, $recording);
+        $recordingLoginForm = $this->createForm(RecordingType::class, $recording);
+        return $this->render('views/controllers/recording/index.html.twig', [
+                'recordingLoginForm' => $recordingLoginForm->createView(),
+            ]
         );
+    }
+
+    /**
+     * @Route("/recording/addRecording", name="recording/addRecording")
+     */
+    public function addRecordingAction(Request $request)
+    {
+
+    }
+
+    private function handleForm(Request $request, Recording $recording)
+    {
+        $recordingLoginForm = $this->createForm(RecordingType::class, $recording);
+
+        $recordingLoginForm->handleRequest($request);
+
+        if($recordingLoginForm->isSubmitted() && $recordingLoginForm->isValid())
+        {
+
+
+            //tutaj przekieruj na dashboard
+//            return $this->render('views/controllers/laps/index.html.twig', []);
+            return new JsonResponse(array('a' => 'b'), 200);
+        }
+        return new JsonResponse(array('c' => 'd'), 400);
+//        return $this->render('views/controllers/registration/editUser.html.twig', [
+//            'userLoginForm' => $recordingLoginForm->createView(),
+//        ]);
     }
     /**
      * @Route("/test", name="test")
---
+++ src/Form/RecordingType.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 22.08.18
+ * Time: 09:53
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\FormTypeInterface;
+class RecordingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title', TextType::class, array(
+
+            'label' => 'Tytuł nagrania',
+            'required' => FALSE,
+            'attr' => [
+                'placeholder' => 'Tytuł nagrania',
+                'class' => 'form-control'
+            ]
+        ));
+
+        $builder->add('recordingLink', TextType::class, array(
+            'label' => 'Link',
+            'required' => TRUE,
+            'attr' => [
+                'placeholder' => 'Link',
+                'class' => 'form-control'
+            ],
+        ));
+
+
+        $builder->add('submit', SubmitType::class, array(
+
+            'label' => 'Dodaj nagranie',
+            'attr' => ['id' => 'addRecordingButton',
+                'class' => 'btn btn-primary submitButton'
+            ]
+        ));
+
+
+    }
+
+
+    public  function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'submit_label' => 'Wyslij zgloszenie',
+                'data_class' => 'App\Entity\Recording',
+                'validation_groups' => ['Default'],
+
+            ]
+        );
+    }
+
+
+}
