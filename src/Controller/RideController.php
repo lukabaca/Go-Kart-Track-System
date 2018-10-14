de49d548e75ceef8961e3be9cd2f34bbabb0610d
lukabaca <lkszblaszczyk@gmail.com>
2018-10-14 23:00:56 +0200

    init

---
+++ src/Controller/RideController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luka
+ * Date: 2018-10-14
+ * Time: 22:56
+ */
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RideController extends Controller
+{
+    /**
+     * @Route("/ride/record/index", name="ride/record/index")
+     */
+    public function recordIndexAction(Request $request)
+    {
+        return $this->render('views/controllers/ride/index.html.twig', []);
+    }
+}
