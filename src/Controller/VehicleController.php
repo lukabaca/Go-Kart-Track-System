944582626ae6164a4eb1a785aa7f5125febbc9fa
lukabaca <lkszblaszczyk@gmail.com>
2018-10-16 20:59:17 +0200

    init files etc

---
+++ src/Controller/VehicleController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Luka
+ * Date: 2018-10-16
+ * Time: 20:50
+ */
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class VehicleController extends Controller
+{
+    /**
+     * @Route("/vehicle/index", name="vehicle/index")
+     */
+    public function indexAction(Request $request) {
+        return $this->render('views/controllers/vehicle/index.html.twig' ,[]);
+    }
+}
