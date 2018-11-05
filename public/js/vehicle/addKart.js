54043afe862f6c1751c835a70d6595e8f75c40f8
lukabaca <lkszblaszczyk@gmail.com>
2018-11-05 13:42:29 +0100

    refactored

--- public/js/vehicle/addKart.js
+++ public/js/vehicle/addKart.js
@@ -1,20 +1,17 @@
 $(document).ready(function () {
     function readURL(input) {
-
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-
             reader.onload = function(e) {
                 $('#imageUploaded').attr('src', e.target.result);
             }
-
             reader.readAsDataURL(input.files[0]);
         }
     }
 
     $("#kart_file").on('change', function(e) {
         e.preventDefault();
-       $('.card').remove();
+        $('.card').remove();
         let content =
             '<div class="card">' +
             '<div class="card-body">' +
@@ -22,7 +19,6 @@ $(document).ready(function () {
             '</div>' +
             '</div>';
         $('.imageArea').append(content);
-        console.log('wrzucono');
         readURL(this);
     });
 });
