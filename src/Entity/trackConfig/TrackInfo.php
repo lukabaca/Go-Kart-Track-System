d96a8967477790a0720d68eb32314e4981dee0bb
lukabaca <lkszblaszczyk@gmail.com>
2018-11-12 11:54:02 +0100

    done view of contact

--- src/Entity/trackConfig/TrackInfo.php
+++ src/Entity/trackConfig/TrackInfo.php
@@ -51,6 +51,26 @@ class TrackInfo
      * @ORM\Column(type="string", length=60)
      */
     private $instagramLink;
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $email;
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
 
     /**
      * @return mixed
