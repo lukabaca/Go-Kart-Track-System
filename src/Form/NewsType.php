fc7e35ca74a2222fde4b8d03041048f7969a8438
lukabaca <lkszblaszczyk@gmail.com>
2018-12-02 18:21:43 +0100

    dashboard improvements

--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -7,6 +7,8 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class NewsType extends AbstractType
 {
@@ -14,22 +16,25 @@ class NewsType extends AbstractType
     {
         $builder->add('description', TextareaType::class, array(
             'label' => FALSE,
-            'required' => TRUE,
+            'required' => FALSE,
             'attr' => [
                 'placeholder' => 'Opis',
                 'class' => 'form-control'
-            ]
+            ],
+            'constraints' => new Callback(array($this, 'validate')),
         ));
         $builder->add('file', FileType::class, array(
             'label' => 'Plik',
             'required' => FALSE,
+            'constraints' => new Callback(array($this, 'validate')),
         ));
 
         $builder->add('submit', SubmitType::class, array(
             'label' => 'Zapisz',
             'attr' => ['id' => 'addTrackInfoButton',
                 'class' => 'btn btn-primary submitButton'
-            ]
+            ],
+
         ));
     }
 
@@ -42,5 +47,18 @@ class NewsType extends AbstractType
                 'validation_groups' => ['Default'],
             ]
         );
+    }
+
+    public function validate($value, ExecutionContextInterface $context)
+    {
+        /** @var \Symfony\Component\Form\Form $form */
+        $form = $context->getRoot();
+        $data = $form->getData();
+        $description = $data->getDescription();
+        $file = $data->getFile();
+        if (!$description && !$file) {
+            $context->buildViolation('Przynajmniej jedno z pól jest wymagane by dodać aktualność')
+                ->addViolation();
+        }
     }
 }
