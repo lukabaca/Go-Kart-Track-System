10c5fd596f51d67cf61e8abb3be4e84cfd711877
lukabaca <lkszblaszczyk@gmail.com>
2018-12-02 20:34:41 +0100

    login controller checked

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,16 +1,6 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Luka
- * Date: 2018-10-03
- * Time: 17:50
- */
-
 namespace App\Controller;
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
